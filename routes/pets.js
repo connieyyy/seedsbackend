00eb0008d0dbd6ced77093402b67fcc6fe1a8fd7
connieyyy <con.yang4098@gmail.com>
2024-07-25 11:52:59 -0700

    updated get and put request routes for pet health

--- routes/pets.js
+++ routes/pets.js
@@ -21,7 +21,7 @@ router.get("/:email", async (req, res) => {
   }
 });
 
-// PATCH: update pet name
+// PUT: update pet name
 router.put("/:email/updatePetName", async (req, res) => {
   const { email } = req.params;
   const { newPetName } = req.body;
@@ -44,6 +44,40 @@ router.put("/:email/updatePetName", async (req, res) => {
 });
 
 // GET: get pet health from db
-// PATCH: update pet health from db
+router.get("/:email/getHealth", async (req, res) => {
+  const { email } = req.params;
+
+  try {
+    const user = await UserData.findOne({ email });
+    if (!user) return res.status(404).send("User not found.");
+    const pet = user.pet[0];
+    if (!pet) return res.status(404).send("Pet not found.");
+    res.json({
+      petHealthLevel: pet.petHealthLevel,
+    });
+  } catch (err) {
+    console.error("Error retrieving pet information", err);
+    res.status(500).send(`Internal server error: ${err.message}`);
+  }
+});
+
+// PUT: update pet health from db
+router.put("/:email/updateHealth", async (req, res) => {
+  const { email } = req.params;
+
+  try {
+    const user = await UserData.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.pet[0].petHealthLevel = Math.min(user.pet[0].petHealthLevel - 1, 100);
+    await user.save();
+
+    res.status(200).json("Pet health updated successfully.");
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
 
 module.exports = router;
