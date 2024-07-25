1ef7173faae133f09e129b778ed84276695d5802
connieyyy <con.yang4098@gmail.com>
2024-07-25 11:08:22 -0700

    updated PUT request

--- routes/pets.js
+++ routes/pets.js
@@ -8,7 +8,6 @@ router.get("/:email", async (req, res) => {
 
   try {
     const user = await UserData.findOne({ email });
-    console.log(user);
     if (!user) return res.status(404).send("User not found.");
     const pet = user.pet[0];
     if (!pet) return res.status(404).send("Pet not found.");
@@ -31,11 +30,15 @@ router.put("/:email/updatePetName", async (req, res) => {
     if (!user) {
       return res.status(404).send("User not found.");
     }
-    user.pet[0].petName = newPetName;
-    await user.save();
-    res.status(201).send("Pet name updated successfully");
+    if (user.pet.length > 0) {
+      user.pet[0].petName = newPetName;
+      await user.save();
+      res.status(200).send("Pet name updated successfully");
+    } else {
+      res.status(404).send("Pet not found.");
+    }
   } catch (err) {
-    console.error("Error updating pet name.", err);
+    console.error("Error updating pet name", err);
     res.status(500).send(`Internal server error: ${err.message}`);
   }
 });
