23231777553ed472368bf0eb7ad1f41b430b0216
connieyyy <con.yang4098@gmail.com>
2024-07-25 20:59:21 -0700

    added delete foodlog route

---
+++ routes/chat.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const UserData = require("../models/userData.js");
+
+module.exports = router;
--- routes/foodlogs.js
+++ routes/foodlogs.js
@@ -17,12 +17,10 @@ router.post("/", async (req, res) => {
     const newFoodLog = {
       date,
       foodName,
-      foodDescription, // Added
-      foodPhotoLink, // Added
+      foodDescription,
+      foodPhotoLink,
     };
-    //add new food log to user's foodlogs
     user.FoodLogs.push(newFoodLog);
-    // save updated user document
     await user.save();
     res.status(201).send("Food entry created successfully");
   } catch (err) {
@@ -45,6 +43,31 @@ router.get("/:email/:date", async (req, res) => {
     res.json(foodLog);
   } catch (err) {
     console.error("Error retrieving food log entry", err);
+    res.status(500).send(`Internal server error: ${err.message}`);
+  }
+});
+
+router.delete("/:email/:foodId", async (req, res) => {
+  const { email, foodId } = req.params;
+
+  try {
+    const user = await UserData.findOne({ email });
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+    const foodLogIndex = user.FoodLogs.findIndex(
+      (log) => log._id.toString() === foodId
+    );
+    if (foodLogIndex === -1) {
+      return res.status(404).send("Food log entry not found.");
+    }
+
+    user.FoodLogs.splice(foodLogIndex, 1);
+    await user.save();
+
+    res.send("Food log entry deleted successfully.");
+  } catch (err) {
+    console.error("Error deleting food entry", err);
     res.status(500).send(`Internal server error: ${err.message}`);
   }
 });
